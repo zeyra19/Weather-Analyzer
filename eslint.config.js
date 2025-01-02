304fec342288919f0eac5fcdf97bc8bd4ba078ba
zeyra19 <156345200+zeyra19@users.noreply.github.com>
2025-01-02 10:41:49 +0300

    Revert "Use weather context"

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{jsx,jsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { WeatherProvider } from './redux/WeatherContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WeatherProvider>
-      <App />
-    </WeatherProvider>
+    <App />
   </StrictMode>,
 )
